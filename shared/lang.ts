fceef837b8d9151be44457010c716dbf746d15b1
Shanks <cjinhuo@qq.com>
2024-08-27 13:09:17 +0800

    chore(blazwitcher): :hammer: update contributing

---
+++ shared/lang.ts
@@ -0,0 +1,19 @@
+const Langs = {
+  'Tab': {
+    'cn': '标签页',
+    'en': 'Tab',
+  },
+  'Bookmark': {
+    'cn': '书签',
+    'en': 'Bookmark',
+  },
+  'History': {
+    'cn': '历史记录',
+    'en': 'History',
+  }
+}
+
+
+const getContent = (key: keyof typeof Langs) => {
+  
+}
