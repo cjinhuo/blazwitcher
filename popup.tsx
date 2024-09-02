f45cb2b5708b2f415f4c99bb583a59a35d471442
Shanks <cjinhuo@qq.com>
2024-09-02 14:25:12 +0800

    chore(blazwitcher): :hammer: remove useless popup codes

--- popup.tsx
+++ popup.tsx
@@ -1,28 +1,5 @@
-import { useEffect } from "react"
-
-function IndexPopup() {
-  useEffect(() => {
-    const SEARCH_WINDOW_WIDTH = 900
-    const SEARCH_WINDOW_HEIGHT = 500
-    chrome.windows.create(
-      {
-        width: SEARCH_WINDOW_WIDTH,
-        height: SEARCH_WINDOW_HEIGHT,
-        // use width instead of availWidth could make it looks more centered
-        left: Math.floor((window.screen.width - SEARCH_WINDOW_WIDTH) / 2),
-        top: Math.floor((window.screen.availHeight - SEARCH_WINDOW_HEIGHT) / 2),
-        focused: true,
-        type: "popup",
-        url: "./sidepanel.html"
-      },
-      (window) => {
-        chrome.storage.session.set({
-          selfWindowId: window.id
-        })
-      }
-    )
-  }, [])
-  return <div>{/* <img src="chrome://favicon/https://github.com/XanderXu/SceneKit-AR-VR-information" /> */}</div>
+function Popup() {
+  return <></>
 }
 
-export default IndexPopup
+export default Popup
