469111052bc314221eb967f4800d2baf46ba44ff
Shanks <cjinhuo@qq.com>
2025-07-22 15:24:13 +0800

    feat(blazwitcher-extension): :sparkles: add active item description

--- packages/blazwitcher-extension/sidepanel/footer/active-item-description.tsx
+++ packages/blazwitcher-extension/sidepanel/footer/active-item-description.tsx
@@ -1,8 +1,28 @@
 import { useAtomValue } from 'jotai'
+import { useMemo } from 'react'
+import { FOOTER_DESCRIPTION_I18N_MAP } from '~shared/constants'
+import { ItemType } from '~shared/types'
+import { getItemType } from '~shared/utils'
 import { activeItemAtom } from '~sidepanel/atom'
+import useI18n from '~sidepanel/hooks/useI18n'
 
 export default function ActiveItemDescription() {
+	const i18n = useI18n()
 	const activeItem = useAtomValue(activeItemAtom)
+	const descriptionKey = useMemo(() => {
+		if (!activeItem) return undefined
+		const itemType = getItemType(activeItem)
+		switch (itemType) {
+			case ItemType.Tab:
+				return i18n(FOOTER_DESCRIPTION_I18N_MAP.tab)
+			case ItemType.Bookmark:
+				return i18n(FOOTER_DESCRIPTION_I18N_MAP.bookmark)
+			case ItemType.History:
+				return i18n(FOOTER_DESCRIPTION_I18N_MAP.history)
+			case ItemType.Plugin:
+				return i18n(FOOTER_DESCRIPTION_I18N_MAP.plugin)
+		}
+	}, [activeItem, i18n])
 	console.log('activeItem', activeItem)
-	return <>test</>
+	return <>{descriptionKey}</>
 }
