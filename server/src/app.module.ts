721ab42e510ecba963adbbd6c9b5f94c9f067d86
Shanks <cjinhuo@qq.com>
2025-10-05 11:39:23 +0800

    fix(server): :bug: fix absolute path error

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common'
 import { APP_GUARD } from '@nestjs/core'
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler'
-import { THROTTLE_LIMIT, THROTTLE_TTL } from 'src/shared/constants'
 import { ArkController } from './modules/ark/ark.controller'
 import { ArkService } from './modules/ark/ark.service'
 import { RootController } from './modules/root/root.controller'
+import { THROTTLE_LIMIT, THROTTLE_TTL } from './shared/constants'
 
 @Module({
 	imports: [
